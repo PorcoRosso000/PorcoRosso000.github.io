02e0ec8f77a476a54c5d2c2e690077e59d655aa0
porcorosso <1332355083@qq.com>
2024-07-23 18:27:50 +0800

    Site updated: 2024-07-23 18:27:48

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,6 +1,6 @@
-// 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://guohaomeng.gitee.io/live2d-widget/";
-//const live2d_path = "/live2d-widget/";
+// live2d_path 参数建议使用绝对路径
+// const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -25,52 +25,31 @@ function loadExternalResource(url, type) {
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
-
-Promise.all([
-	loadExternalResource(live2d_path + "waifu.css", "css"),
-	loadExternalResource(live2d_path + "live2d.min.js", "js"),
-	loadExternalResource(live2d_path + "waifu-tips.js", "js")
-]).then(() => {
-	initWidget({
-		waifuPath: live2d_path + "waifu-tips.json",
-		 //apiPath: "https://live2d.fghrsh.net/api/",
-		apiPath: "https://api.zsq.im/live2d/",  //多模型api，25个角色
-		//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-		cdnPath: false
+if (screen.width >= 768) {
+	Promise.all([
+		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+	]).then(() => {
+		// 配置选项的具体用法见 README.md
+		initWidget({
+			waifuPath: live2d_path + "waifu-tips.json",
+			//apiPath: "https://live2d.fghrsh.net/api/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
+		});
 	});
-});
-// initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
-// API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
-// 初始化看板娘会自动加载指定目录下的 waifu-tips.json
-//自动适应手机与pc
-function autoResize(){
-	var wH = window.innerWidth;
-	var x = document.getElementById("waifu");
-	//console.log(wH);
-	if (wH < 768) {
-		x.style.transformOrigin = "0 100%";
-		x.style.transform = "scale(0.3)";
-	}
-	else{
-		x.style.transformOrigin = "0 100%";
-		x.style.transform = "scale(0.76)";
-	}
-
 }
 
-window.onresize=function(){  
-	//console.log('监听变化')
-	autoResize();
-}
 console.log(`
   く__,.ヘヽ.        /  ,ー､ 〉
            ＼ ', !-─‐-i  /  /´
-           ／｀ｰ'  meng L/／｀ヽ､
+           ／｀ｰ'       L/／｀ヽ､
          /   ／,   /|   ,   ,       ',
        ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
         ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
           !,/7 '0'     ´0iソ|    |
-          |.从"     ^    ,,,, / |./    |
+          |.从"    _     ,,,, / |./    |
           ﾚ'| i＞.､,,__  _,.イ /   .i   |
             ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
               | |/i 〈|/   i  ,.ﾍ |  i  |
